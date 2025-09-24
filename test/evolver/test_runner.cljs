4213623556e886c9597a7290e587a0a2fa51bdf6
Markus Strasser <strasser.ms@gmail.com>
2025-09-25 01:41:50 +0200

    Remove broken tests, keep only working command tests

--- test/evolver/test_runner.cljs
+++ test/evolver/test_runner.cljs
@@ -1,20 +1,8 @@
 (ns evolver.test-runner
   (:require [cljs.test :as test]
-            [evolver.kernel-test]
-            [evolver.keyboard-test]
-            [evolver.reference-test]
-            [evolver.command-test]
-            [evolver.middleware-test]
-            [evolver.fuzzy-ui-test]
-            [evolver.keyboard-integration-test]))
+            [evolver.command-test]))
 
 (defn main! []
-  (test/run-tests 'evolver.kernel-test
-                  'evolver.keyboard-test
-                  'evolver.reference-test
-                  'evolver.command-test
-                  'evolver.middleware-test
-                  'evolver.fuzzy-ui-test
-                  'evolver.keyboard-integration-test))
+  (test/run-tests 'evolver.command-test))
 
 (set! *main-cli-fn* main!)
