e0f9d73d53210f03cf8063f3b78fcba96c8c1225
Markus Strasser <strasser.ms@gmail.com>
2025-09-25 00:11:10 +0200

    fix: keyboard handler logic and double execution
    - Fix critical nil return bug in handle-keyboard-event
    - Remove inline command execution to prevent double dispatch
    - Return commands for proper registry dispatch

--- src/evolver/keyboard.cljs
+++ src/evolver/keyboard.cljs
@@ -1,6 +1,6 @@
 (ns evolver.keyboard
   (:require [evolver.kernel :as kernel]
-            [evolver.commands :as commands]))
+            [evolver.registry :as registry]))
 
 (defn key-matches?
   "Check if a key event matches the given criteria"
@@ -18,80 +18,93 @@
         selected (first selected-set)]
     {:selected-set selected-set :selected selected}))
 
-;; Keyboard mapping configuration
+;; Generate keyboard mappings from registry
+(defn generate-keyboard-mappings
+  "Generate keyboard mappings from command registry"
+  []
+  (let [registry-mappings (registry/get-keyboard-mappings)]
+    (concat
+     ;; Registry-based mappings
+     (for [[key [cmd-id params]] registry-mappings]
+       {:key key
+        :command [cmd-id params]})
+
+     ;; Complex mappings with conditions
+     [{:key "Escape"
+       :command [:clear-selection]}
+
+      {:key "Backspace"
+       :requires-selection true
+       :command [:delete-selected-blocks]}
+
+      {:key "Delete"
+       :requires-selection true
+       :command [:delete-selected-blocks]}
+
+      {:key "Enter"
+       :requires-selection true
+       :command [:create-child-block]}
+
+      {:key "Enter" :shift true
+       :requires-selection true
+       :command [:create-sibling-above]}
+
+      {:key "A" :shift true :meta true
+       :command [:select-all-blocks]}
+
+      {:key "A" :shift true :ctrl true
+       :command [:select-all-blocks]}
+
+      {:key "ArrowDown" :alt true
+       :requires-selection true
+       :command [:navigate-sibling {:direction :down}]}
+
+      {:key "ArrowUp" :alt true
+       :requires-selection true
+       :command [:navigate-sibling {:direction :up}]}
+
+      {:key "a" :meta true
+       :requires-selection true
+       :command [:select-parent]}
+
+      {:key "a" :ctrl true
+       :requires-selection true
+       :command [:select-parent]}
+
+      {:key "Tab"
+       :requires-selection true
+       :command [:indent-block]}
+
+      {:key "Tab" :shift true
+       :requires-selection true
+       :command [:outdent-block]}
+
+      {:key "ArrowUp" :alt true :shift true
+       :requires-selection true
+       :command [:move-block {:direction :up}]}
+
+      {:key "ArrowDown" :alt true :shift true
+       :requires-selection true
+       :command [:move-block {:direction :down}]}
+
+      {:key "." :meta true
+       :requires-selection true
+       :command [:toggle-collapse]}
+
+      {:key "." :ctrl true
+       :requires-selection true
+       :command [:toggle-collapse]}])))
+
 (def keyboard-mappings
   "Declarative keyboard mappings with conditions and commands"
-  [{:key "Escape"
-    :command [:clear-selection]}
-
-   {:key "Backspace"
-    :requires-selection true
-    :command [:delete-selected-blocks]}
-
-   {:key "Delete"
-    :requires-selection true
-    :command [:delete-selected-blocks]}
-
-   {:key "Enter"
-    :requires-selection true
-    :command [:create-child-block]}
-
-   {:key "Enter" :shift true
-    :requires-selection true
-    :command [:create-sibling-above]}
-
-   {:key "A" :shift true :meta true
-    :command [:select-all-blocks]}
-
-   {:key "A" :shift true :ctrl true
-    :command [:select-all-blocks]}
-
-   {:key "ArrowDown" :alt true
-    :requires-selection true
-    :command [:navigate-sibling {:direction :down}]}
-
-   {:key "ArrowUp" :alt true
-    :requires-selection true
-    :command [:navigate-sibling {:direction :up}]}
-
-   {:key "a" :meta true
-    :requires-selection true
-    :command [:select-parent]}
-
-   {:key "a" :ctrl true
-    :requires-selection true
-    :command [:select-parent]}
-
-   {:key "Tab"
-    :requires-selection true
-    :command [:indent-block]}
-
-   {:key "Tab" :shift true
-    :requires-selection true
-    :command [:outdent-block]}
-
-   {:key "ArrowUp" :alt true :shift true
-    :requires-selection true
-    :command [:move-block {:direction :up}]}
-
-   {:key "ArrowDown" :alt true :shift true
-    :requires-selection true
-    :command [:move-block {:direction :down}]}
-
-   {:key "." :meta true
-    :requires-selection true
-    :command [:toggle-collapse]}
-
-   {:key "." :ctrl true
-    :requires-selection true
-    :command [:toggle-collapse]}])
+  (vec (generate-keyboard-mappings)))
 
 (defn handle-keyboard-event
-  "Handle keyboard events using declarative mappings and command dispatch"
+  "Handle keyboard events using declarative mappings"
   [store event]
   (let [selection-state (get-current-selection store)]
     (loop [mappings keyboard-mappings]
-      (when-let [mapping (first mappings)]
+      (if-let [mapping (first mappings)]
         (if (and (key-matches? event mapping)
                  (or (not (:requires-selection mapping))
                      (not-empty (:selected-set selection-state))))
@@ -100,7 +113,9 @@
                     cmd-params (if (seq params)
                                  (if (= 1 (count params)) (first params) params)
                                  {})]
-                (commands/dispatch-command store {:keyboard-event event} [cmd-name cmd-params]))
-              true)
-          (recur (rest mappings)))))
-    false))
+                (js/console.log "Executing keyboard command:" cmd-name cmd-params)
+                ;; Return the command for dispatch to the command registry
+                [cmd-name cmd-params]))
+          (recur (rest mappings)))
+        ;; Return false when no command found
+        false))))
