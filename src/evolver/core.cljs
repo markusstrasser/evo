414824846cd3e457cd329cd4d2b598f2ddd0176f
Markus Strasser <strasser.ms@gmail.com>
2025-09-25 17:12:29 +0200

    feat(core): wire store, event dispatch and renderer; integrate kernel ops

--- src/evolver/core.cljs
+++ src/evolver/core.cljs
@@ -1,17 +1,44 @@
 (ns evolver.core
-  (:require [replicant.dom :as r]))
+  (:require [replicant.dom :as r]
+            [evolver.kernel :as kernel]
+            [evolver.renderer :as renderer]))
 
-(defonce app-state (atom {:counter 5}))
+(defonce store (atom kernel/db))
+
+(defn handle-event [event-data actions]
+  (let [actions (if (sequential? actions) actions [actions])]
+    (doseq [action actions]
+      (case (first action)
+        :select-node
+        (let [{:keys [node-id]} (second action)]
+          (swap! store assoc-in [:view :selected] #{node-id}))
+
+        :set-selected-op
+        (let [value (.. (:replicant/dom-event event-data) -target -value)]
+          (swap! store assoc :selected-op (keyword value)))
+
+        :apply-selected-op
+        (when-let [op (:selected-op @store)]
+          (swap! store
+                 (case op
+                   :create-child-block kernel/create-child-block
+                   :create-sibling-above kernel/create-sibling-above
+                   :create-sibling-below kernel/create-sibling-below
+                   :indent kernel/indent
+                   :outdent kernel/outdent
+                   identity)))
+
+        nil))))
 
 (defn ^:export main []
   (r/set-dispatch!
     (fn [event-data handler-data]
-      ;; Handle events here
-      ))
+      (when (= :replicant.trigger/dom-event (:replicant/trigger event-data))
+        (handle-event event-data handler-data))))
   ;; Initial render
   (let [root (.getElementById js/document "root")]
-    (r/render root [:div "App State: " @app-state])
+    (r/render root (renderer/render @store))
 
-    (add-watch app-state :render
-               (fn [key atom old-state new-state]
-                 (r/render root [:div "App State: " new-state])))))
+    (add-watch store :render
+               (fn [_ _ _ new-state]
+                 (r/render root (renderer/render new-state))))))
