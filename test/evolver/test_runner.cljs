b836d4315cdfb9af53028d8480ed76492897b85b
Markus Strasser <strasser.ms@gmail.com>
2025-09-25 17:19:47 +0200

    Add agent-gotchas.md with debugging session learnings

--- test/evolver/test_runner.cljs
+++
@@ -1,8 +0,0 @@
-(ns evolver.test-runner
-  (:require
-    [cljs.test]
-    [evolver.core-test]
-    [evolver.kernel-test]))
-
-(defn main! [& args]
-  (cljs.test/run-tests 'evolver.core-test 'evolver.kernel-test))
