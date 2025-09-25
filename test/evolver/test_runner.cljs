4dfff3eedc47f00521eaf0668a0950f41c93c8ca
Markus Strasser <strasser.ms@gmail.com>
2025-09-25 20:22:21 +0200

    Temporarily disable agent tests due to compilation issues
    - Comment out agent test requirements in test runner
    - Agent namespaces not available in node-test build
    - Allows main tests to run while agent tests are fixed

--- test/evolver/test_runner.cljs
+++ test/evolver/test_runner.cljs
@@ -1,8 +1,12 @@
 (ns evolver.test-runner
   (:require [cljs.test :as test]
-            [evolver.kernel-test]))
+            [evolver.kernel-test]
+            ;; [agent.code-analysis-test] ;; TODO: Fix compilation issue
+            ))
 
 (defn main! []
-  (test/run-tests 'evolver.kernel-test))
+  (test/run-tests 'evolver.kernel-test
+                  ;; 'agent.code-analysis-test
+                  ))
 
 (set! *main-cli-fn* main!)
