b285ab7174ba892b379a09c75b9e344838b71482
Markus Strasser <strasser.ms@gmail.com>
2025-09-26 14:57:06 +0200

    refactor: replace nondeterministic set-based selection with ordered cursor + selection vector

--- src/evolver/commands.cljs
+++ src/evolver/commands.cljs
@@ -4,11 +4,24 @@
 
 ;; ---------- 0) Tiny utilities ----------
 (defn db [store] @store)
-(defn sel [store] (or (get-in (db store) [:view :selected]) #{}))
-(defn sel1 [store] (first (sel store)))
+(defn selv [store] (get-in (db store) [:view :selection] []))
+(defn selset [store] (get-in (db store) [:view :selection-set] #{}))
+(defn cursor [store] (get-in (db store) [:view :cursor]))
 (defn pos-of [store id] (K/node-position (db store) id))
-(defn set-sel! [store s] (swap! store assoc-in [:view :selected] s))
-(defn toggle-in-set [S x] ((if (contains? S x) disj conj) S x))
+
+(defn set-selection! [store v]
+  (swap! store assoc-in [:view :selection] v)
+  (swap! store assoc-in [:view :selection-set] (set v))
+  (swap! store assoc-in [:view :cursor] (peek v))) ; last clicked = primary
+
+(defn toggle-in-ordered [v x]
+  (if (some #{x} v) (vec (remove #{x} v)) (conj v x)))
+
+(defn toggle-in-set [s x]
+  (if (contains? s x) (disj s x) (conj s x)))
+
+(defn- sort-by-index [db ids]
+  (->> ids (sort-by #(-> (K/node-position db %) :index))))
 
 (defn apply! [store cmd]
   (when cmd
@@ -22,57 +35,58 @@
      :stop! #(.stopPropagation e)}))
 
 (defn ctx [store]
-  (let [s (sel store)]
+  (let [s (selv store)]
     {:db (db store)
+     :cursor (cursor store)
      :selected s
      :selected1 (first s)
      :pos (fn [id] (K/node-position (db store) id))}))
 
 ;; ---------- 1) Pure intent → command builders ----------
-(def intent->cmd
+(def intent->command
   {;; creation
    :create-child-block
-   (fn [{:keys [selected1]}]
-     (when selected1
+   (fn [{:keys [cursor]}]
+     (when cursor
        {:op :insert
-        :parent-id selected1
+        :parent-id cursor
         :node-id (K/gen-new-id)
         :node-data {:type :div :props {:text "New child"}}
         :position nil}))
 
    :create-sibling-above
-   (fn [{:keys [selected1 pos]}]
-     (when selected1
-       (let [{:keys [parent index]} (pos selected1)]
+   (fn [{:keys [cursor pos]}]
+     (when cursor
+       (let [{:keys [parent index]} (pos cursor)]
          {:op :insert :parent-id parent :node-id (K/gen-new-id)
           :node-data {:type :div :props {:text "New sibling"}}
           :position index})))
 
    :create-sibling-below
-   (fn [{:keys [selected1 pos]}]
-     (when selected1
-       (let [{:keys [parent index]} (pos selected1)]
+   (fn [{:keys [cursor pos]}]
+     (when cursor
+       (let [{:keys [parent index]} (pos cursor)]
          {:op :insert :parent-id parent :node-id (K/gen-new-id)
           :node-data {:type :div :props {:text "New sibling"}}
           :position (inc index)})))
 
    ;; structure moves
    :indent
-   (fn [{:keys [selected1 pos]}]
-     (when selected1
-       (let [{:keys [index parent children]} (pos selected1)]
+   (fn [{:keys [cursor pos]}]
+     (when cursor
+       (let [{:keys [index parent children]} (pos cursor)]
          (when (> index 0)
-           {:op :move :node-id selected1
+           {:op :move :node-id cursor
             :new-parent-id (nth children (dec index))
             :position nil}))))
 
    :outdent
-   (fn [{:keys [selected1 pos]}]
-     (when selected1
-       (let [{:keys [parent]} (pos selected1)]
-         (when (not= parent "root")
+   (fn [{:keys [cursor pos]}]
+     (when cursor
+       (let [{:keys [parent]} (pos cursor)]
+         (when (not= parent K/root-id)
            (let [{p2 :parent i2 :index} (pos parent)]
-             {:op :move :node-id selected1
+             {:op :move :node-id cursor
               :new-parent-id p2
               :position (inc i2)})))))
 
@@ -91,75 +105,77 @@
 
    ;; reorder within parent
    :move-up
-   (fn [{:keys [selected1 pos db]}]
-     (when selected1
-       (let [{:keys [parent index]} (pos selected1)
+   (fn [{:keys [cursor pos db]}]
+     (when cursor
+       (let [{:keys [parent index]} (pos cursor)
              new (max 0 (dec index))]
          (when (not= index new)
-           {:op :reorder :node-id selected1 :parent-id parent
+           {:op :reorder :node-id cursor :parent-id parent
             :from-index index :to-index new}))))
 
    :move-down
-   (fn [{:keys [selected1 pos db]}]
-     (when selected1
-       (let [{:keys [parent index]} (pos selected1)
+   (fn [{:keys [cursor pos db]}]
+     (when cursor
+       (let [{:keys [parent index]} (pos cursor)
              sibs (get-in db [:children-by-parent parent])
              new (min (dec (count sibs)) (inc index))]
          (when (not= index new)
-           {:op :reorder :node-id selected1 :parent-id parent
-            :from-index index :to-index new}))))
+           {:op :reorder :node-id cursor :parent-id parent
+            :from-index index :to-index new}))))})
 
-   ;; navigation helpers that return navigation info instead of commands
+(def intent->nav
+  {;; navigation helpers that return navigation targets
    :nav-up
-   (fn [{:keys [selected1 db]}]
-     (when selected1
-       (K/get-prev db selected1)))
+   (fn [{:keys [cursor db]}]
+     (when cursor
+       (K/get-prev db cursor)))
 
    :nav-down
-   (fn [{:keys [selected1 db]}]
-     (when selected1
-       (K/get-next db selected1)))
+   (fn [{:keys [cursor db]}]
+     (when cursor
+       (K/get-next db cursor)))
 
    :nav-sibling-up
-   (fn [{:keys [selected1 pos db]}]
-     (when selected1
-       (let [{:keys [parent index]} (pos selected1)]
+   (fn [{:keys [cursor pos db]}]
+     (when cursor
+       (let [{:keys [parent index]} (pos cursor)]
          (when (> index 0)
            (let [siblings (get-in db [:children-by-parent parent])]
              (nth siblings (dec index)))))))
 
    :nav-sibling-down
-   (fn [{:keys [selected1 pos db]}]
-     (when selected1
-       (let [{:keys [parent index]} (pos selected1)
+   (fn [{:keys [cursor pos db]}]
+     (when cursor
+       (let [{:keys [parent index]} (pos cursor)
              siblings (get-in db [:children-by-parent parent])]
          (when (< (inc index) (count siblings))
            (nth siblings (inc index))))))
 
    :nav-parent
-   (fn [{:keys [selected1 db]}]
-     (when selected1
-       (let [parent-id (K/get-parent db selected1)]
-         (when (and parent-id (not= parent-id "root"))
+   (fn [{:keys [cursor db]}]
+     (when cursor
+       (let [parent-id (K/get-parent db cursor)]
+         (when (and parent-id (not= parent-id K/root-id))
            parent-id))))
 
    :nav-first-child
-   (fn [{:keys [selected1 db]}]
-     (when selected1
-       (K/get-first-child db selected1)))
+   (fn [{:keys [cursor db]}]
+     (when cursor
+       (K/get-first-child db cursor)))
 
    :nav-last-child
-   (fn [{:keys [selected1 db]}]
-     (when selected1
-       (K/get-last-child db selected1)))})
+   (fn [{:keys [cursor db]}]
+     (when cursor
+       (K/get-last-child db cursor)))})
 
 ;; ---------- 2) Thin UI handlers (effects kept here on purpose) ----------
 (defn select-node [store event {:keys [node-id]}]
-  (when-let [{:keys [shift? ctrl? meta? stop!]} (modifiers event)] (stop!))
-  (let [cur (sel store)
-        chord? (let [{:keys [shift? ctrl? meta?]} (modifiers event)] (or shift? ctrl? meta?))
-        next (if chord? (toggle-in-set cur node-id) #{node-id})]
-    (swap! store assoc-in [:view :selected] next)
+  (let [m (modifiers event)
+        _ (when m ((:stop! m)))
+        chord? (when m (or (:shift? m) (:ctrl? m) (:meta? m)))
+        v (selv store)
+        next (if chord? (toggle-in-ordered v node-id) [node-id])]
+    (set-selection! store next)
     (swap! store K/log-operation {:op :select-node :node-id node-id})))
 
 (defn set-selected-op [store event _]
@@ -168,7 +184,7 @@
 
 (defn apply-selected-op [store _ _]
   (when-let [op (:selected-op (db store))]
-    (when-let [build (intent->cmd op)]
+    (when-let [build (intent->command op)]
       (apply! store (build (ctx store))))))
 
 (defn hover-node [store _event {:keys [node-id]}]
@@ -184,7 +200,7 @@
 ;; Navigation helpers that update selection
 (defn navigate-to [store target-node]
   (when target-node
-    (set-sel! store #{target-node})))
+    (set-selection! store [target-node])))
 
 ;; ---------- 3) Registry (now tiny & consistent) ----------
 (def command-registry
@@ -196,59 +212,59 @@
    :undo undo!
    :redo redo!
 
-   ;; view-only helpers
-   :clear-selection (fn [s _ _] (set-sel! s #{}))
-   :select-all-blocks (fn [s _ _] (set-sel! s (set (keys (:nodes (db s))))))
+    ;; view-only helpers
+   :clear-selection (fn [s _ _] (set-selection! s []))
+   :select-all-blocks (fn [s _ _] (set-selection! s (vec (keys (:nodes (db s))))))
 
-   ;; navigation commands (use nav helpers from intent->cmd)
+    ;; navigation commands (use nav helpers from intent->nav)
    :navigate-sequential (fn [s _ {:keys [direction]}]
-                          (let [build (intent->cmd (case direction :up :nav-up :down :nav-down))]
+                          (let [build (intent->nav (case direction :up :nav-up :down :nav-down))]
                             (navigate-to s (build (ctx s)))))
 
    :navigate-sibling (fn [s _ {:keys [direction]}]
-                       (let [build (intent->cmd (case direction :up :nav-sibling-up :down :nav-sibling-down))]
+                       (let [build (intent->nav (case direction :up :nav-sibling-up :down :nav-sibling-down))]
                          (navigate-to s (build (ctx s)))))
 
    :select-parent (fn [s _ _]
-                    (navigate-to s ((intent->cmd :nav-parent) (ctx s))))
+                    (navigate-to s ((intent->nav :nav-parent) (ctx s))))
 
    :select-first-child (fn [s _ _]
-                         (navigate-to s ((intent->cmd :nav-first-child) (ctx s))))
+                         (navigate-to s ((intent->nav :nav-first-child) (ctx s))))
 
    :select-last-child (fn [s _ _]
-                        (navigate-to s ((intent->cmd :nav-last-child) (ctx s))))
+                        (navigate-to s ((intent->nav :nav-last-child) (ctx s))))
 
-   ;; direct intents (single node operations)
-   :create-child-block (fn [s _ _] (apply! s ((intent->cmd :create-child-block) (ctx s))))
-   :create-sibling-above (fn [s _ _] (apply! s ((intent->cmd :create-sibling-above) (ctx s))))
-   :create-sibling-below (fn [s _ _] (apply! s ((intent->cmd :create-sibling-below) (ctx s))))
+    ;; direct intents (single node operations)
+   :create-child-block (fn [s _ _] (apply! s ((intent->command :create-child-block) (ctx s))))
+   :create-sibling-above (fn [s _ _] (apply! s ((intent->command :create-sibling-above) (ctx s))))
+   :create-sibling-below (fn [s _ _] (apply! s ((intent->command :create-sibling-below) (ctx s))))
    :move-block (fn [s _ {:keys [direction]}]
                  (let [k (case direction :up :move-up :down :move-down)]
-                   (apply! s ((intent->cmd k) (ctx s)))))
+                   (apply! s ((intent->command k) (ctx s)))))
 
-   ;; multi-node structural operations
+    ;; multi-node structural operations
    :indent-block (fn [s _ _]
-                   (doseq [id (sel s)]
-                     (let [build (intent->cmd :indent)
-                           cmd (build (assoc (ctx s) :selected1 id))]
-                       (when cmd (apply! s cmd)))))
+                   (let [ids (sort-by-index (db s) (selv s))]
+                     (doseq [id ids]
+                       (when-let [cmd ((intent->command :indent) (assoc (ctx s) :cursor id))]
+                         (apply! s cmd)))))
 
    :outdent-block (fn [s _ _]
-                    (doseq [id (sel s)]
-                      (let [build (intent->cmd :outdent)
-                            cmd (build (assoc (ctx s) :selected1 id))]
-                        (when cmd (apply! s cmd)))))
+                    (let [ids (sort-by-index (db s) (selv s))]
+                      (doseq [id ids]
+                        (when-let [cmd ((intent->command :outdent) (assoc (ctx s) :cursor id))]
+                          (apply! s cmd)))))
 
-   ;; multi-node operations
+    ;; multi-node operations
    :delete-selected-blocks
    (fn [s _ _]
-     (doseq [id (sel s)]
+     (doseq [id (selv s)]
        (apply! s {:op :delete :node-id id :recursive true}))
-     (set-sel! s #{}))
+     (set-selection! s []))
 
-   ;; toggle view state
+    ;; toggle view state
    :toggle-collapse (fn [s _ _]
-                      (let [selected (sel1 s)]
+                      (let [selected (cursor s)]
                         (when selected
                           (let [current-collapsed (get-in @s [:view :collapsed] #{})
                                 new-collapsed (toggle-in-set current-collapsed selected)]
