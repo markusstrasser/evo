df8175dd9e5c5f2484ce4aae91e2bd81afdcb4cd
Markus Strasser <strasser.ms@gmail.com>
2025-12-16 20:32:14 -0800

    refactor: remove orphaned plugin manifest system
    plugin_manifest_runtime.cljs loaded plugins.edn but nothing consumed the manifest. Abandoned declarative registry pattern.

--- src/shell/plugin_manifest_runtime.cljs
+++
@@ -1,7 +0,0 @@
-(ns shell.plugin-manifest-runtime
-  (:require [cljs.reader :as reader]
-            [shadow.resource :as resource]))
-
-(def manifest
-  (-> (resource/inline "plugins.edn")
-      reader/read-string))
