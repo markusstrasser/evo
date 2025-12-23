374f7e8d6595929bfa8d4d0a2a4b1e2f7876a9f8
Markus Strasser <strasser.ms@gmail.com>
2025-12-22 21:24:43 -0800

    chore: remove unused shell/demo_data.cljs (7 LOC)
    Namespace loaded seed-data.edn but was never required by any other module.

--- src/shell/demo_data.cljs
+++
@@ -1,7 +0,0 @@
-(ns shell.demo-data
-  (:require [cljs.reader :as reader]
-            [shadow.resource :as resource]))
-
-(def ops
-  (-> (resource/inline "seed-data.edn")
-      reader/read-string))
